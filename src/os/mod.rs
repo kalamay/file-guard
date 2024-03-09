ddc3fbfe356e2762a821402364eb9efdde00ddc6
Jeremy Larkin <jeremylarkin@gmail.com>
2024-03-09 03:55:41 -0800

    remove cfg-if and update deps

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,13 +1,13 @@
 //! Provides low-level support operations for file locking.
-cfg_if::cfg_if! {
-    if #[cfg(windows)] {
-        pub mod windows;
-        pub use self::windows::{raw_file_lock, raw_file_downgrade};
-    } else if #[cfg(unix)] {
-        #[macro_use]
-        pub mod unix;
-        pub use self::unix::{raw_file_lock, raw_file_downgrade};
-    } else {
-        // Unknown target_family
-    }
-}
+#[cfg(windows)]
+pub mod windows;
+
+#[cfg(windows)]
+pub use self::windows::{raw_file_lock, raw_file_downgrade};
+
+#[cfg(unix)]
+#[macro_use]
+pub mod unix;
+
+#[cfg(unix)]
+pub use self::unix::{raw_file_lock, raw_file_downgrade};
